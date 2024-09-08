55555d484365501e720b8cb88d32f85ea81aef8d
Aly Khaled <alykhaled936@gmail.com>
2024-09-09 02:17:05 +0300

    add Bestseller listView & custom scroll view

---
+++ lib/Features/home/presentation/view/widgets/best_seller_list_view.dart
@@ -0,0 +1,35 @@
+import 'package:bookly_app/Features/home/presentation/view/widgets/best_seller_item.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SliverList(
+          delegate: SliverChildBuilderDelegate(
+        childCount: 10,
+        (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: BestSellerItem(),
+          );
+        },
+      )),
+    );
+
+    // return Expanded(
+    //   child: ListView.builder(
+    //       physics: const NeverScrollableScrollPhysics(),
+    //       padding: EdgeInsets.zero,
+    //       itemCount: 10,
+    //       itemBuilder: (context, index) {
+    //         return const Padding(
+    //           padding: EdgeInsets.symmetric(vertical: 5),
+    //           child: BestSellerItem(),
+    //         );
+    //       }),
+    // );
+  }
+}
--- lib/Features/home/presentation/view/widgets/home_view_body.dart
+++ lib/Features/home/presentation/view/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/Features/home/presentation/view/widgets/best_seller_item.dart';
+import 'package:bookly_app/Features/home/presentation/view/widgets/best_seller_list_view.dart';
 import 'package:bookly_app/Features/home/presentation/view/widgets/books_list_view.dart';
 import 'package:bookly_app/Features/home/presentation/view/widgets/custom_app_bar.dart';
 import 'package:bookly_app/core/utils/styles.dart';
@@ -9,19 +9,25 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-        BooksListView(),
-        Padding(
-          padding: EdgeInsets.only(left: 24,bottom: 20),
-          child: Text(
-            "Best Seller",
-            style: Styles.textStyle18,
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              BooksListView(),
+              Padding(
+                padding: EdgeInsets.only(left: 24, bottom: 20),
+                child: Text(
+                  "Best Seller",
+                  style: Styles.textStyle18,
+                ),
+              ),
+            ],
           ),
         ),
-        BestSellerItem(),
+        BestSellerListView()
       ],
     );
   }
